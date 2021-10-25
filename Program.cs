c31b7a65851bd9305243154b9ba0675378b9e377
RSabiRr <92287092+RSabiRr@users.noreply.github.com>
2021-10-26 01:07:21 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,184 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ////////////1-dən 100-ə qədər ədədləri console-da çap edin
+
+
+            //for (int i = 1; i <= 100; i++)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            //for (int i = 0; i <= 100; i++)
+            //{
+            //    if (i % 2 == 0)
+            //    {
+            //        continue;
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //}
+
+
+
+
+
+            //1-dən 100-ə qədər tək ədədləri console-da çap edin
+
+            //for (int i = 1; i <= 100; i++)
+            //{
+            //    if(i%2 == 0)
+            //    {
+            //        continue;
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //}
+
+
+
+
+            //User tərəfindən daxil edilmiş ədədin 2 mislini(2-yə vurmaq) console-da çap edin
+
+            // Console.WriteLine("Ededi daxil edin");
+            //int a= Convert.ToInt32(Console.ReadLine());
+            // int b;
+            // b = a * 2;
+            // Console.WriteLine("Hasil: "+b);
+            // Console.ReadKey();
+
+
+
+
+
+
+            //Write a program in C# to display the first 10 natural numbers
+
+            //for (int i = 1; i <=10; i++)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+
+
+
+
+            //Write a C# program to find the sum of first 10 natural number
+
+            //int i, sum = 0;
+
+            //for (i = 1; i <= 10; i++)
+            //{
+
+            //    sum = sum + i;
+
+            //}
+
+            //Console.WriteLine("Cavab  " + sum);
+
+
+
+
+
+
+            // Write a C# program that takes the radius of a circle as input and calculate the perimeter and area of the circle;
+
+            //Console.WriteLine("Radiusu daxil edin: ");
+            //int a = Convert.ToInt32(Console.ReadLine());
+
+            //var per = 2 * Math.PI * a;
+            //var sahe = Math.PI * (Math.Pow(a, 2));
+
+            //Console.WriteLine("Perimetr " + per);
+
+            //Console.WriteLine("Sahe " + sahe);
+
+
+
+
+            //Write a C# program that takes two numbers as input and perform an operation (+, -, x, /) on them and display the result of that operation.
+
+            //Console.WriteLine("1 ci ededi daxil edin: ");
+
+            //int a = Convert.ToInt32(Console.ReadLine());
+
+            //Console.WriteLine("2 ci ededi daxil edin: ");
+            //int b = Convert.ToInt32(Console.ReadLine());
+
+            //var cem = a + b;
+            //var ferq = a - b;
+            //var bolunme = a / b;
+            //var vurma = a * b;
+            //Console.WriteLine("Cem:  " + cem);
+            //Console.WriteLine("Ferq:  " + ferq);
+            //Console.WriteLine("Bolme:  " + bolunme);
+            //Console.WriteLine("Vurma:  " + vurma);
+
+            ////////////////////////////////
+
+            //for (int i = 0; i < 6; i++)
+            //{
+            //    int a = Convert.ToInt32(Console.ReadLine());
+
+
+            //    Console.WriteLine(a);
+
+            //}
+
+            //////////////////////////////////////
+
+
+            ///Write a C# program that takes userid and password as input. After 3 wrong attems, user will be rejected.
+
+            //int count = 0;
+            //string user,password;
+
+            //do
+            //{
+            //    Console.WriteLine("Istifadecinin adi: ");
+            //     user=Console.ReadLine();
+
+            //    Console.WriteLine("Parol : ");
+            //     password=Console.ReadLine();
+
+            //    count++;
+
+            //}
+            //while (((user != "Admin" || password != "Admin123"))
+            //  && (count != 3));
+
+            //if (count == 3)
+            //{
+            //    Console.Write("Hesab muvveqeti bloklandi ! ");
+            //    Console.ReadKey();
+            //}
+            //else
+            //{
+            //    Console.Write("Hesaba daxil oldunuz  ");
+            //    Console.ReadKey();
+            //}
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
